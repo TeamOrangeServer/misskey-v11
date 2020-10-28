39cf2811231b4c76ada384983eabfab3f2fb6e4a
mei23 <m@m544.net>
2020-10-29 05:59:10 +0900

    delete message

---
+++ src/services/messages/delete.ts
@@ -0,0 +1,28 @@
+import User, { isLocalUser, isRemoteUser } from '../../models/user';
+import { publishMessagingStream } from '../stream';
+import MessagingMessage, { IMessagingMessage } from '../../models/messaging-message';
+import { renderActivity } from '../../remote/activitypub/renderer';
+import { renderTombstone } from '../../remote/activitypub/renderer/tombstone';
+import { deliver } from '../../queue';
+import config from '../../config';
+
+export async function deleteMessage(message: IMessagingMessage) {
+	await MessagingMessage.remove({ _id: message._id });
+
+	const user = await User.findOne({ _id: message.userId});
+	const recipient = await User.findOne({ _id: message.recipientId});
+	if (user == null || recipient == null) return;
+
+	if (isLocalUser(user)) {
+		publishMessagingStream(user._id, message.recipientId, 'deleted', message._id);
+	}
+
+	if (isLocalUser(recipient)) {
+		publishMessagingStream(recipient._id, message.userId, 'deleted', message._id);
+	}
+
+	if (isLocalUser(user) && isRemoteUser(recipient)) {
+		const activity = renderActivity(renderTombstone(`${config.url}/notes/${message._id}`));
+		deliver(user, activity, recipient.inbox);
+	}
+}
